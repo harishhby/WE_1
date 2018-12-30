2bab44c93e2d5089417a02f40f6bfa55c48d4eef
harishhby <harish.hby@gmail.com>
2018-12-30 17:16:23 +0530

    Second commit

---
+++ Demo_2.py
@@ -0,0 +1,14 @@
+class Test:
+    x=100#public-Accessed across all level
+    _y=200#protected-Accessed in parent and child class
+    __z=300#private-Accessed only in same class
+    def m1(self):
+        print(Test.x)
+        print(Test._y)
+        print(Test.__z)
+t=Test()
+t.m1()
+print("-------------")
+print(Test.x)
+print(Test._y)
+print(Test.__z)
